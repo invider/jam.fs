9a067bbcc310079cc041e57bff480e3e37194592
Igor Khotin <khotin@gmx.com>
2019-01-27 01:26:55 +0200

    [*] blinking text updated

--- core/ext/dna/text/blinkingText.js
+++ core/ext/dna/text/blinkingText.js
@@ -1,14 +1,14 @@
 let BlinkingText = function(dat) {
+    this.align ="center";
+    this.font = '72px zekton'
+    this.fillStyle = '#FFA000'
+    
+    sys.augment(this, dat)
+
     if (dat.rx) this.x = dat.rx/100 * ctx.width
     else this.x = dat.x
     if (dat.ry) this.y = dat.ry/100 * ctx.height
     else this.y = dat.y
-
-    this.txt = dat.txt
-
-    this.align ="center";
-    this.font = '72px zekton'
-    this.fillStyle = '#FFA000'
 }
 
 BlinkingText.prototype.evo = function(dt) {
